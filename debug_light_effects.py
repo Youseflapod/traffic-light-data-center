a4e6761c02b9bfd372aced7462876c7fbc842ebc
Yousef Abdelgaber <yabdelgaber@gmail.com>
2021-01-25 22:04:50 -0500

    d

--- debug_light_effects.py
+++ debug_light_effects.py
@@ -8,7 +8,7 @@ currEffect = 0
 
 def debug_press(channel):
     global currEffect
-    print(f'Current Effect: {currEffect}')
+    print(f'Current Effect: {int(round(currEffect))}')
     leff.start(int(round(currEffect)))
     currEffect += 0.5
     if currEffect == leff.DEMO_MODE:
