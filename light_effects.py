7fd59fab69849e6623c6a4e079694c01f617eb8a
Yousef Abdelgaber <yabdelgaber@gmail.com>
2021-01-27 03:25:36 -0500

    Update light_effects.py

--- light_effects.py
+++ light_effects.py
@@ -141,7 +141,7 @@ def run_light_thread():
     elif effect == PAST_BEDTIME:
         intv = 0.18 # s
         pause = 0.20 
-        cooldown = 1.0
+        cooldown = 0.3
         while effect == PAST_BEDTIME:
             set_light_calib_rgba(c.INTERRUPTION_L_B)
             sleep(intv)
